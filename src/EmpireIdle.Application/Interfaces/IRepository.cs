51376502ccde786f8c0951bcb91de45e94e99f05
Сергій Охріменко <serhiy0072@gmail.com>
2026-04-14 21:48:34 +0300

    add IRepository, IVillageRepository, IPlayerRepository, IPlayerWalletRepository, IUnitOfWork

---
+++ src/EmpireIdle.Application/Interfaces/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using EmpireIdle.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmpireIdle.Application.Interfaces
+{
+    /// <summary>
+    /// Базовий інтерфейс репозиторію для всіх агрегатів.
+    /// </summary>
+    public interface IRepository<T> where T : Entity
+    {
+        /// <summary>Знайти за ідентифікатором.</summary>
+        Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+        /// <summary>Додати новий запис.</summary>
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
+
+        /// <summary>Оновити існуючий запис.</summary>
+        void Update(T entity);
+
+        // <summary>Видалити запис.</summary>
+        void Delete(T entity);
+    }
+}
---
+++ src/EmpireIdle.Application/Interfaces/IVillageRepository.cs
@@ -0,0 +1,15 @@
+﻿using EmpireIdle.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmpireIdle.Application.Interfaces
+{
+    /// <summary>
+    /// Репозиторій для роботи з Village aggregate.
+    /// </summary>
+    public interface IVillageRepository : IRepository<Village>
+    {
+        /// <summary>Знайти село за ідентифікатором гравця.</summary>
+        Task<Village?> GetByPlayerIdAsync(Guid playerId, CancellationToken cancellationToken = default);
+    }
+}
